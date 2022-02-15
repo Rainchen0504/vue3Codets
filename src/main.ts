1b65a4c70af35cec79a2fe7e103102a1f4b6eeaf
张雨晨 <15596686748@163.com>
2022-02-15 10:31:37 +0800

    vuex初始化判断token

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,11 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import { setupStore } from './store'
+
 const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(ElementPlus)
+setupStore()
 app.mount('#app')
