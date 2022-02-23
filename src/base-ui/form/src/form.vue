3608ac664d1d0ef79233adc314f927a0ffcf7224
张雨晨 <15596686748@163.com>
2022-02-23 17:54:24 +0800

    封装表单组件

--- src/base-ui/form/src/form.vue
+++ src/base-ui/form/src/form.vue
@@ -1,23 +1,102 @@
 <template>
   <div class="hy-form">
+    <!-- 头部插槽 -->
     <div class="header">
       <slot name="header"></slot>
     </div>
     <!-- 表单封装 -->
-    <el-form :label-width="labelWidth">
+    <el-form :label-width="props.labelWidth">
       <el-row>
-        <template v-for="item in formItems" :key="item.label">
+        <template v-for="item in props.formItems" :key="item.label">
           <!-- 布局配置，响应式格栅属性，即不同分辨率下每行分为多少份 -->
-          <el-col v-bind="colLayout">
-            <el-form-item :label="item.label" :rules="item.rules" :style="itemStyle">
+          <el-col v-bind="props.colLayout">
+            <el-form-item :label="item.label" :rules="item.rules" :style="props.itemStyle">
+              <template v-if="item.type === 'input' || item.type === 'password'">
+                <el-input
+                  :placeholder="item.placeholder"
+                  v-bind="item.otherOptions"
+                  :show-password="item.type === 'password'"
+                  v-model="formData[`${item.field}`]"
+                />
+              </template>
+              <template v-else-if="item.type === 'select'">
+                <el-select
+                  :placeholder="item.palceholder"
+                  v-bind="item.otherOptions"
+                  style="width: 100%"
+                  v-model="formData[`${item.field}`]"
+                >
+                  <el-option
+                    v-for="option in item.options"
+                    :key="option.value"
+                    :value="option.value"
+                    >{{ option.title }}</el-option
+                  >
+                </el-select>
+              </template>
+              <template v-else-if="item.type === 'datepicker'">
+                <el-date-picker
+                  style="width: 100%"
+                  v-bind="item.otherOptions"
+                  v-model="formData[`${item.field}`]"
+                ></el-date-picker>
+              </template>
             </el-form-item>
           </el-col>
         </template>
       </el-row>
     </el-form>
+    <!-- 底部插槽 -->
+    <div class="footer">
+      <slot name="footer"></slot>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch, PropType } from 'vue'
+import { IFormItem } from '../types'
+
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    required: true
+  },
+  formItems: {
+    type: Array as PropType<IFormItem[]>,
+    default: () => []
+  },
+  labelWidth: {
+    type: String,
+    default: '100px'
+  },
+  itemStyle: {
+    type: Object,
+    default: () => ({ padding: '10px 40px' })
+  },
+  colLayout: {
+    type: Object,
+    default: () => ({
+      xl: 6,
+      lg: 8,
+      md: 12,
+      sm: 24,
+      xs: 24
+    })
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const formData = ref({ ...props.modelValue })
+
+watch(
+  formData,
+  (newValue) => {
+    emit('update:modelValue', newValue)
+  },
+  { deep: true }
+)
+</script>
 
 <style scoped></style>
